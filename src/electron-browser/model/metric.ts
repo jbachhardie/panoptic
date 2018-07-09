b5040168281524c6be65f61f04246c23791c6e7b
Jae Anne Bach Hardie <jhardie@findmypast.com>
2018-07-09 16:03:38 +0100

    :construction: add stats

--- src/electron-browser/model/metric.ts
+++
@@ -1,6 +0,0 @@
-import { observable } from "mobx"
-
-export class Metric {
-  public id: string
-  @observable.shallow public data: Array<{ x: number; y: number }> = []
-}
--- src/electron-browser/model/service.ts
+++ src/electron-browser/model/service.ts
@@ -1,7 +1,6 @@
 import { ipcRenderer } from "electron"
 import { action, observable } from "mobx"
-import { forEachObjIndexed, pipe } from "ramda"
-import { Metric } from "./metric"
+import * as R from "ramda"
 
 export interface ICodeLocation {
   line: number
@@ -16,6 +15,15 @@ export interface IDiagnostic {
   additionalInfo?: string
 }
 
+export interface IPerformanceStats {
+  cpu: NodeJS.CpuUsage
+  memory: NodeJS.MemoryUsage
+}
+
+export interface ILogMessage {
+  message: any[]
+}
+
 export type LogLevel = "debug" | "info" | "warn" | "error"
 
 export interface ILog {
@@ -27,31 +35,63 @@ export interface IServiceProps {
   name: string
 }
 
+export interface IDataPoint {
+  x: number
+  y: number
+}
+
 export class Service {
   public name: string
   @observable.shallow public diagnostics: IDiagnostic[] = []
   @observable.shallow public logs: ILog[] = []
-  @observable.shallow public cpuMetrics: Metric[] = []
-  @observable.shallow public memoryMetrics: Metric[] = []
+
+  @observable
+  public stats = {
+    cpu: {
+      system: [] as IDataPoint[],
+      user: [] as IDataPoint[],
+    },
+    memory: {
+      external: [] as IDataPoint[],
+      heapTotal: [] as IDataPoint[],
+      heapUsed: [] as IDataPoint[],
+      rss: [] as IDataPoint[],
+    },
+  }
 
   constructor({ name }: IServiceProps) {
     this.name = name
     ipcRenderer.on(name, (_: any, { eventName, ...args }: any) => {
       return {
-        "log-debug": ({ message }) => this.log("debug", message),
-        "log-error": ({ message }) => this.log("error", message),
-        "log-info": ({ message }) => this.log("info", message),
-        "log-raw": ({ message }) => this.log("info", message),
-        "log-warn": ({ message }) => this.log("warn", message),
-        stats: ({ cpu, memory }) => forEachObjIndexed(),
+        "log-debug": ({ message }: ILogMessage) => this.log("debug", message),
+        "log-error": ({ message }: ILogMessage) => this.log("error", message),
+        "log-info": ({ message }: ILogMessage) => this.log("info", message),
+        "log-raw": ({ message }: ILogMessage) => this.log("info", message),
+        "log-warn": ({ message }: ILogMessage) => this.log("warn", message),
+        stats: this.updateStats,
       }[eventName](args)
     })
     ipcRenderer.send("service:start", name)
   }
 
   @action.bound
-  private stats({ cpu, memory }: IPerformanceStats) {
-    this.performanceStats.push({ timestamp: new Date(), cpu, memory })
+  private updateStats(stats: IPerformanceStats) {
+    R.forEachObjIndexed(
+      (categoryStats, categoryName) =>
+        R.forEachObjIndexed((metricValue, metricName) => {
+          const metric: IDataPoint[] | undefined = R.path(
+            [categoryName, metricName],
+            this.stats
+          )
+          if (metric) {
+            metric.push({
+              x: new Date().getTime(),
+              y: metricValue,
+            })
+          }
+        }, categoryStats),
+      stats
+    )
   }
 
   @action.bound
